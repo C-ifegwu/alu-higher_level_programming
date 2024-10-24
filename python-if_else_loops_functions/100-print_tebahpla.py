e3188d8a32e2df34f16f8b0e21a58638a4dd8bfe
C-ifegwu <c.ifegwu@alustudent.com>
2024-10-24 01:23:01 -0700

    all

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
-# Print the ASCII alphabet in reverse order, alternating lowercase and uppercase
-print("".join("{:c}".format(i if i % 2 == 0 else i - 32) for i in range(122, 96, -1)), end="")
+for i in range(122, 96, -1):
+    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
