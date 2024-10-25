3c6b0ee7dba2c741f1180c099695c0b1ab61a62b
C-ifegwu <c.ifegwu@alustudent.com>
2024-10-25 02:34:50 -0700

    all

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 import sys
 from calculator_1 import add, sub, mul, div
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-elif operator == '/':
-    if b == 0:
-        print("Error: Division by zero")
+
+if __name__ == "__main__":
+    # Check for valid input (3 arguments)
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    result = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
-print(f"{a} {operator} {b} = {result}")
+
+    # Extract the arguments
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    # Perform the operation based on the operator
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        if b == 0:
+            print("Error: Division by zero")
+            sys.exit(1)
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    # Print the result
+    print(f"{a} {operator} {b} = {result}")
