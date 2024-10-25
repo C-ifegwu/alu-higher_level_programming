f0b69a365ddc749153b3c3d1760f156e51053a9a
C-ifegwu <c.ifegwu@alustudent.com>
2024-10-25 02:41:46 -0700

    all

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
-def magic_calculation(a, b):
+def magic_calculation(a, b, c):
+    from magic_calculation_102 import add, sub
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
@@ -8,8 +9,3 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-if __name__ == "__main__":
-    result1 = magic_calculation(3, 5)
-    print("Result of magic_calculation(3, 5):", result1)
-    result2 = magic_calculation(6, 5)
-    print("Result of magic_calculation(6, 5):", result2)
