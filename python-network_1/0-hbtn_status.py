c3d97f5c1f76dabf09676d8d9a9820c9f52fffdb
C-ifegwu <c.ifegwu@alustudent.com>
2024-11-24 07:43:03 -0800

    interesting

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,13 +3,10 @@
 - fetches https://alu-intranet.hbtn.io/status.
 - uses urlib package
 """
-
 import urllib.request
-
 url = 'https://intranet.hbtn.io/status'
 if url.startswith('https://'):
     url = 'https://alu-intranet.hbtn.io/status'
-
 if __name__ == '__main__':
     with urllib.request.urlopen(url) as res:
         content = res.read()
